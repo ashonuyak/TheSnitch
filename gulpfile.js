9c6b4f7a146ae5c4cf3d6329b76cbee6e5295b8b
ashonuyak <ashonuyak@gmail.com>
2021-11-23 19:37:02 +0200

    almost end

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,19 @@ const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
 function style() {
-  return gulp.src('./src/scss/*.scss')
+  return gulp
+    .src('./src/scss/*.scss')
     .pipe(sass())
-    .pipe(autoprefixer({
-      cascade: false
-    }))
-    .pipe(cleanCSS({
-      compatibility: 'ie8'
-    }))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      }),
+    )
+    .pipe(
+      cleanCSS({
+        compatibility: 'ie8',
+      }),
+    )
     .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream());
 }
@@ -29,7 +34,8 @@ function style() {
 // }
 
 function nunjucks() {
-  return gulp.src('./src/nunjucks/*.html')
+  return gulp
+    .src('./src/nunjucks/*.html')
     .pipe(njk.compile())
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream());
@@ -44,9 +50,9 @@ async function build() {
 function watch() {
   browserSync.init({
     server: {
-      baseDir: "./dist",
-      index: 'index.html'
-    }
+      baseDir: './dist',
+      index: 'index.html',
+    },
   });
   gulp.watch('./src/scss/*.scss', style);
   // gulp.watch('./src/pug/*.pug', template);
